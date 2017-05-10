aa6a3b064b15b86a04aadea1fa1187e78d383536
Tim Espasandin <tim.espasandin@gmail.com>
2017-05-10 10:25:23 -0400

    Added comments

--- src/Question2Class.java
+++ src/Question2Class.java
@@ -1,9 +1,11 @@
 
 public class Question2Class {
 	
+	// Declare properties
 	static int x;
 	static int y;
 	
+	// Returns sum of x and y.
 	public static int GetSum(){
 		return x+y;
 	}
--- src/Question2Routine.java
+++ src/Question2Routine.java
@@ -1,6 +1,7 @@
 
 public class Question2Routine {
 
+	// Assigns contents of command line arguments to Question2Class's x and y and displays the sum.
 	public static void main(String[] args){
 		Question2Class.x = Integer.parseInt(args[0]);
 		Question2Class.y = Integer.parseInt(args[1]);
